0ab987547bbb1d6e145d1ad105cafaf6b9464ee0
NETRASG <netra4al@gmail.com>
2022-10-16 16:56:16 +0530

    payment section

--- test/Locators/elementAction.ts
+++ test/Locators/elementAction.ts
@@ -1,14 +1,22 @@
-class Actions{
-
-    async setText(locator:ChainablePromiseElement<WebdriverIO.Element>,inputText:string){
+class Actions {
+    async setText(locator: ChainablePromiseElement<WebdriverIO.Element>, inputText: string) {
         await (await locator).setValue(inputText);
     }
-
-    async Click(locator:ChainablePromiseElement<WebdriverIO.Element>){
+    async Click(locator: ChainablePromiseElement<WebdriverIO.Element>) {
         await (await locator).click();
     }
- 
+    async LocatorArray(locator: ChainablePromiseElement<WebdriverIO.Element>, name: string) {
+        for (let i = 0; i < (await locator).length; i++) {
+            const eachElement = await locator[i].getText();
+        
+            if (eachElement.includes(name)) {
+                await this.Click(await locator[i])
+                break;
+            }
+        }
+    }
+    async GetText(locator: ChainablePromiseElement<WebdriverIO.Element>) {
+        await (await locator).getText();
+    }
 }
 export default new Actions()
-
-
--- test/specs/amazonE2E.ts
+++ test/specs/amazonE2E.ts
@@ -1,5 +1,6 @@
 import E2Emethod from '../amazoneE2EMethods/amazoneE2EMethods'
 import data from '../resources/userCredential.json'
+import filterData from '../resources/filterData.json'
 describe("Amazone shopping site", () => {
     it("Open web app using appUrl", async () => {
         await E2Emethod.appOpen()
@@ -7,11 +8,24 @@ describe("Amazone shopping site", () => {
     it("get sign in into the app using phone number and password", async () => {
         await E2Emethod.sigIn(data.phonenum, data.password);
     })
-
-    it("product filtering",async () => {
-        await E2Emethod.productFilter();
-        console.log("filter")
-        
+    it("product filtering", async () => {
+        await E2Emethod.categoryFilter(filterData.categoty, filterData.subCategory);
+        await E2Emethod.specificProductFilter(filterData.brand);
+        await E2Emethod.specificProductFilter(filterData.rating);
+        await E2Emethod.specificProductFilter(filterData.price);
+        await E2Emethod.specificProductFilter(filterData.ItemCondition);
     })
-
+    it("open desired product and add to cart", async () => {
+        await E2Emethod.selectSpecificProduct(filterData.productName);
+    })
+    it("go to cart and proceed to buy", async () => {
+        await E2Emethod.cartItems();
+        await E2Emethod.proceedToBuy()
+    })
+    it("address", async () => {
+        await E2Emethod.address()
+    })
+    it("select Payment Option", async () => {
+        await E2Emethod.payMentOption(filterData.bank)
+    })
 })
